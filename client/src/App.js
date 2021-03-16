69a7368f2b7843d9cae6370a7ff478a5d91266f8
Orkhan Rustamli <orkhan.rustamli.96@gmail.com>
2021-03-16 10:43:04 +0200

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Fib from "./Fib";
+import OtherPage from "./otherPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edittt <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/otherPage.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const otherPage = () => {
+  return (
+    <div>
+      Im some other page!
+      <Link to="/">Go back home!</Link>
+    </div>
+  );
+};
+
+export default otherPage;
